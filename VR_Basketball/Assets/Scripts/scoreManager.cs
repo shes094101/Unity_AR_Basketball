63757244e3a43e41d52f014f19d539c767bcb42d
shes094101 <32055777+shes094101@users.noreply.github.com>
2020-12-22 11:59:27 +0800

    second

---
+++ VR_Basketball/Assets/Scripts/scoreManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text text;
+    [Header("分數")]
+    public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
